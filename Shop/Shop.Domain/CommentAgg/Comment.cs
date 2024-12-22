05bc8af002bf90f3d5d9288fd5c8c4aab98c3ae4
Mahdi Ebrahimi <mahdi78eb@gmail.com>
2024-12-22 21:47:17 +0330

    # Completed Comment Agg

---
+++ Shop/Shop.Domain/CommentAgg/Comment.cs
@@ -0,0 +1,47 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+using Shop.Domain.CommentAgg.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.CommentAgg
+{
+    public class Comment : AggregateRoot
+    {
+        public long UserId { get; private set; }
+        public string Text { get; private set; }
+        public long ProductId { get; private set; }
+        public DateTime UpdateDate { get; private set; }
+        public CommentStatus Status { get; private set; }
+
+        public Comment(long userId, string text, long productId)
+        {
+            Guard(text);
+
+            UserId = userId;
+            Text = text;
+            ProductId = productId;
+            Status = CommentStatus.Pennding;
+        }
+
+        public void Edit(string text)
+        {
+            Guard(text);
+
+            Text = text;
+            UpdateDate = DateTime.Now;
+        }
+        public void ChangeStatus(CommentStatus status)
+        {
+            Status = status;
+            UpdateDate = DateTime.Now;
+        }
+        public void Guard(string text)
+        {
+            NullOrEmptyDomainDataException.CheckString(text, nameof(text));
+        }
+    }
+}
---
+++ Shop/Shop.Domain/CommentAgg/Enums/CommentStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Shop.Domain.CommentAgg.Enums
+{
+    public enum CommentStatus
+    {
+        Pennding,
+        Accepted,
+        Rejected
+    }
+}
