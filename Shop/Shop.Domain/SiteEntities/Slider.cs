74ee702c1421ad50efe927a406edc45e187514ff
Mahdi Ebrahimi <mahdi78eb@gmail.com>
2024-12-22 23:06:10 +0330

    # Completed Site Entities Banner & Slider

---
+++ Shop/Shop.Domain/SiteEntities/Slider.cs
@@ -0,0 +1,43 @@
+﻿using Common.Domain;
+using Common.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Domain.SiteEntities
+{
+    public class Slider : BaseEntity
+    {
+        public string Title { get; private set; }
+        public string Link { get; private set; }
+        public string ImageName { get; private set; }
+
+        public Slider(string title, string link, string imageName)
+        {
+            Guard(title, imageName, link);
+
+            Title = title;
+            Link = link;
+            ImageName = imageName;
+        }
+
+        public void Edit(string title, string imageName, string link)
+        {
+            Guard(title, imageName, link);
+
+            Title = title;
+            Link = link;
+            ImageName = imageName;
+        }
+
+        public void Guard(string title, string imageName, string link)
+        {
+            NullOrEmptyDomainDataException.CheckString(title, nameof(title));
+            NullOrEmptyDomainDataException.CheckString(imageName, nameof(imageName));
+            NullOrEmptyDomainDataException.CheckString(link, nameof(link));
+
+        }
+    }
+}
