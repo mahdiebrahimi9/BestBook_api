c2310c3624b4743c8c3b8e8ed8661e75974baf37
Mahdi Ebrahimi <mahdi78eb@gmail.com>
2025-01-24 23:13:57 +0330

    # Added Product Edit Application

---
+++ Shop/Shop.Application/Products/Edit/EditProductCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Common.Application;
+using Shop.Domain.ProductAgg.Services;
+using Shop.Domain.ProductAgg.Repository;
+
+namespace Shop.Application.Products.Edit
+{
+    internal class EditProductCommandHandler : IBaseCommandHandler<EditProductCommand>
+    {
+        private readonly IProductDomainService _productDomain;
+        private readonly IProductRepository _productRepository;
+
+        public EditProductCommandHandler(IProductDomainService productDomain, IProductRepository productRepository)
+        {
+            _productDomain = productDomain;
+            _productRepository = productRepository;
+        }
+
+        public async Task<OperationResult> Handle(EditProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetTracking(request.CategoryId);
+            if (product == null)
+            {
+                return OperationResult.NotFound();
+            }
+        }
+    }
+}
---
+++ Shop/Shop.Application/Products/Edit/EditProductCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using Common.Application.Validation.FluentValidations;
+using Common.Application.Validation;
+using FluentValidation;
+
+namespace Shop.Application.Products.Edit
+{
+    public class EditProductCommandValidator : AbstractValidator<EditProductCommand>
+    {
+        public EditProductCommandValidator()
+        {
+            RuleFor(r => r.Title)
+               .NotEmpty()
+               .WithMessage(ValidationMessages.required("عنوان"));
+
+            RuleFor(r => r.Description)
+            .NotEmpty()
+            .WithMessage(ValidationMessages.required("توضیحات"));
+
+            RuleFor(r => r.Slug)
+            .NotEmpty()
+            .WithMessage(ValidationMessages.required("slug"));
+
+            RuleFor(r => r.ImageFile)
+                .JustImageFile();
+        }
+    }
+}
